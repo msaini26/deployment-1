457a79c66d92fd4eecf1bb77a11b5f927f403a9c
Mansi S <mansisaini26@gmail.com>
2024-02-14 00:23:30 -0800

    scene flow

--- src/SceneFlow.js
+++ src/SceneFlow.js
@@ -7,7 +7,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { Button, CardActionArea, CardActions, Stack } from "@mui/material";
 import sceneFlowOne from "./assets/Scene Flow.png";
-import sceneFlowTwo from "./assets/Scene Flow Prototype 2.png"
+import sceneFlowTwo from "./assets/Scene Flow Prototype 2.png";
 
 function LightBulbIcon(props) {
   return (
@@ -20,7 +20,7 @@ function LightBulbIcon(props) {
 export default function SceneFlow() {
   return (
     <Stack spacing={2} direction="column" sx={{ mb: 5 }}>
-      <Card sx={{ maxWidth: '100%' }}>
+      <Card sx={{ maxWidth: "100%" }}>
         <CardMedia
           component="img"
           height="100%"
@@ -29,18 +29,36 @@ export default function SceneFlow() {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Scene Flow Prototype #1
+            Scene Flow Lo-fi Prototype #1
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            The GIF above depicts an interactive prototype developed in Unity.
-            You can see the player's pull movement towards the orange cylinder
-            on the screen. As the player moves towards the object, the player's
-            gravity will be rotated to match the new setting.
+            The Figma scene flow prototype, which was created in Figma, shows
+            the gameplay loops as if the elevator was not the main core
+            component of the game. The elevator is mainly used as a means to
+            transition between each level/floor and is also used as a quick
+            break for the player to rest. The main concept is that once you
+            leave the elevator, you cannot go back inside and must find the exit
+            elevator to leave the floor. Through this, we want to test how
+            gameplay will be affected by not providing a safe space for the
+            player to run back to while trying to find the next exit. Without
+            providing a safe space, we also think that this will save up a lot
+            of time on the actual core gameplay because they wouldn’t have the
+            option to continuously run back to a specific location. This would
+            force the player to proceed forward and continue to explore what’s
+            ahead.
           </Typography>
         </CardContent>
+        <CardActions>
+          <Button
+            size="medium"
+            href="https://www.figma.com/file/hJwAMEw4vAEteGg13QZVST/Scene-Flow-Prototype-1?type=whiteboard&node-id=0%3A1&t=e1mznuJ6DbGly71K-1"
+          >
+            View Full Prototype
+          </Button>
+        </CardActions>
       </Card>
 
-      <Card sx={{ maxWidth: '100%'}}>
+      <Card sx={{ maxWidth: "100%" }}>
         <CardMedia
           component="img"
           height="100%"
@@ -49,15 +67,42 @@ export default function SceneFlow() {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Push-and-Pull Mechanic Result #2
+            Scene Flow Lo-fi Prototype #2
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            The GIF above depicts a Unity prototype with the player's utilization of the walls and
-            orange cylinder to progress further in the game. As the player pulls
-            themselves towards the orange cylinder, their gravity is rotated
-            once again to match the new setting.
+            This scene flow FigJam prototype explains the core gameplay logic as
+            if the elevator was the core root of the game itself. Essentially,
+            the player can run back into the elevator at any point during the
+            game without losing their game progress or being affected by any
+            enemies that may cross their path. This game flow is especially
+            helpful for beginners or new players who want to “test the waters”
+            of the game without diving headfirst into the experience. The
+            initial game flow will remain the same where a title screen,
+            instruction page, and settings area are included for the player.
+            Once the players transition to the game, they will spawn inside of
+            the “safe elevator” with the option to select a level. As the player
+            progresses through the game, more level options will be added,
+            allowing them to either move to the next level or replay some of the
+            older challenges. Suppose it’s the player’s first time playing the
+            game. In that case, the first level will be a tutorial allowing the
+            player to discover the game mechanics and movement functionalities
+            they have access to. Also, the elevator buttons will be round
+            rectangular buttons with a mysterious scripture and an English
+            translation titled “Level X: Name of the Level” where each level
+            name is unique. In addition, by making the elevator the main game
+            root, the gameplay experience might be more leisurely as the player
+            can take their time exploring the levels and taking breaks when
+            needed.
           </Typography>
         </CardContent>
+        <CardActions>
+          <Button
+            size="medium"
+            href="https://www.figma.com/file/To9jX7TeacyNwx92G6ZzRV/Scene-Flow-Prototype-2?type=whiteboard&node-id=1%3A2&t=LZdU8L6tSAib1JnV-1"
+          >
+            View Full Prototype
+          </Button>
+        </CardActions>
       </Card>
     </Stack>
   );
